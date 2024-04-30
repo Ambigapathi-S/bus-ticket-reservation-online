255c8efa08cf2622f233b169674986d25f9d6b53
Ambigapathi-S <rajibca20@gmail.com>
2024-04-30 09:31:29 +0530

    Swagger Title changes

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,11 +1,34 @@
 package com.example.backend;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Online Bus Ticket Booking Application API Documentation",
+				description = "Online Bus Ticket Booking Application API Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Ambiga",
+						email = "ambiga.bca2020@gmail.com",
+						url = "https://ambigapathi.netlify.app/"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot REST API for Online Bus Ticket Booking Application"
+		)
+)
 public class BackendApplication {
 
 	@Bean
