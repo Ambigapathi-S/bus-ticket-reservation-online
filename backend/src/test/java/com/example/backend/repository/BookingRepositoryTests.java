975e898eb0ae3d4b93475cfbfe71658815a38a50
Ambigapathi-S <rajibca20@gmail.com>
2024-04-30 09:49:11 +0530

    Missing test cases updated

--- backend/src/test/java/com/example/backend/repository/BookingRepositoryTests.java
+++ backend/src/test/java/com/example/backend/repository/BookingRepositoryTests.java
@@ -1,4 +1,126 @@
 package com.example.backend.repository;
 
+import com.example.backend.entity.Booking;
+import com.example.backend.entity.Bus;
+import com.example.backend.entity.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest
 public class BookingRepositoryTests {
+    @Autowired
+    private BookingRepository bookingRepository;
+    private UserRepository userRepository;
+    private BusRepository busRepository;
+    private ModelMapper modelMapper;
+    private Booking booking;
+    @BeforeEach
+    public void setUp() {
+        User user = new User();
+        user.setName("Ambiga");
+        user.setEmail("ambiga@gmail.com");
+        user.setUsername("Ambiga");
+        user.setPassword("Ambiga@123$");
+
+        Bus bus = new Bus();
+        bus.setName("Nellai Exp");
+        bus.setSource("Tirunelveli");
+        bus.setDestination("Chennai");
+        bus.setDepartureTime("19:00");
+        bus.setArrivalTime("06:00");
+        bus.setSeats(50);
+        bus.setAmenities("Ac, Wifi");
+        bus.setFare(500.00);
+        bus.setOperators("Book Bus");
+
+        booking = new Booking();
+        booking.setBookingDate("2024-05-01");
+        booking.setNumSeats(5);
+        booking.setTotalAmount(5000.00);
+        booking.setStatus("pending");
+        booking.setPaymentDetails("card_1234_23232_2323");
+        booking.setUser(user);
+        booking.setBus(bus);
+    }
+    @Test
+    public void givenBookingObject_whenSave_thenReturnSavedBooking() {
+        Booking savedBooking = bookingRepository.save(booking);
+
+        // Checking - assertion
+        assertThat(savedBooking).isNotNull();
+        assertThat(savedBooking.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void givenBookingList_whenFindAll_thenReturnBookingList() {
+        bookingRepository.save(booking);
+        Booking booking2 = new Booking();
+        User user = new User();
+        user.setName("Raji");
+        user.setEmail("raji@gmail.com");
+        user.setUsername("Raji");
+        user.setPassword("Raji@123$");
+
+        Bus bus = new Bus();
+        bus.setName("Chennai Exp");
+        bus.setSource("Chennai");
+        bus.setDestination("Mumbai");
+        bus.setDepartureTime("19:00");
+        bus.setArrivalTime("06:00");
+        bus.setSeats(50);
+        bus.setAmenities("Ac, Wifi");
+        bus.setFare(500.00);
+        bus.setOperators("Book Bus");
+
+        booking2 = new Booking();
+        booking2.setBookingDate("2024-05-02");
+        booking2.setNumSeats(5);
+        booking2.setTotalAmount(5000.00);
+        booking2.setStatus("pending");
+        booking2.setPaymentDetails("card_1234_23232_2323");
+        booking2.setUser(user);
+        booking2.setBus(bus);
+        bookingRepository.save(booking2);
+        List<Booking> bookingList = bookingRepository.findAll();
+
+        // Checking - assertion
+        assertThat(bookingList).isNotNull();
+        assertThat(bookingList.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void givenBookingObject_whenFindById_thenReturnBookingObject() {
+        Booking savedBooking = bookingRepository.save(booking);
+        Booking bookingDb = bookingRepository.findById(savedBooking.getId()).get();
+        // Checking - assertion
+        assertThat(bookingDb).isNotNull();
+    }
+
+    @Test
+    public void givenBookingObject_whenUpdateBooking_thenReturnUpdatedBookingObject() {
+        Booking savedBooking = bookingRepository.save(booking);
+        Booking bookingDb = bookingRepository.findById(savedBooking.getId()).get();
+        bookingDb.setBookingDate("2024-05-02");
+        Booking updatedBooking = bookingRepository.save(bookingDb);
+        // Checking - assertion
+        assertThat(updatedBooking.getBookingDate()).isEqualTo("2024-05-02");
+    }
+
+    @Test
+    public void givenBookingObject_whenDeleteBooking_thenRemoveBooking() {
+        Booking savedBooking = bookingRepository.save(booking);
+        bookingRepository.deleteById(savedBooking.getId());
+
+        Optional<Booking> bookingOptional = bookingRepository.findById(savedBooking.getId());
+
+        // Checking - assertion
+        assertThat(bookingOptional).isEmpty();
+    }
 }
--- backend/src/test/java/com/example/backend/repository/BusRepositoryTests.java
+++ backend/src/test/java/com/example/backend/repository/BusRepositoryTests.java
@@ -1,4 +1,88 @@
 package com.example.backend.repository;
+import com.example.backend.entity.Bus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+import java.util.Optional;
+
+@DataJpaTest
 public class BusRepositoryTests {
+    @Autowired
+    private BusRepository busRepository;
+    private Bus bus;
+    @BeforeEach
+    public void setUp() {
+        bus = new Bus();
+        bus.setName("Nellai Exp");
+        bus.setSource("Tirunelveli");
+        bus.setDestination("Chennai");
+        bus.setDepartureTime("19:00");
+        bus.setArrivalTime("06:00");
+        bus.setSeats(50);
+        bus.setAmenities("Ac, Wifi");
+        bus.setFare(500.00);
+        bus.setOperators("Book Bus");
+    }
+    @Test
+    public void givenBusObject_whenSave_thenReturnSavedBus() {
+        Bus savedBus = busRepository.save(bus);
+
+        // Checking - assertion
+        assertThat(savedBus).isNotNull();
+        assertThat(savedBus.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void givenBusList_whenFindAll_thenReturnBusList() {
+        busRepository.save(bus);
+        Bus bus2 = new Bus();
+        bus.setName("Chennai Exp");
+        bus.setSource("Chennai");
+        bus.setDestination("Tvl");
+        bus.setDepartureTime("05:00");
+        bus.setArrivalTime("12:00");
+        bus.setSeats(50);
+        bus.setAmenities("Ac, Wifi");
+        bus.setFare(500.00);
+        bus.setOperators("Book Bus");
+        busRepository.save(bus2);
+        List<Bus> busList = busRepository.findAll();
+
+        // Checking - assertion
+        assertThat(busList).isNotNull();
+        assertThat(busList.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void givenBusObject_whenFindById_thenReturnBusObject() {
+        Bus savedBus = busRepository.save(bus);
+        Bus busDb = busRepository.findById(savedBus.getId()).get();
+        // Checking - assertion
+        assertThat(busDb).isNotNull();
+    }
+
+    @Test
+    public void givenBusObject_whenUpdateBus_thenReturnUpdatedBusObject() {
+        Bus savedBus = busRepository.save(bus);
+        Bus busDb = busRepository.findById(savedBus.getId()).get();
+        busDb.setName("Nellai Express");
+        Bus updatedBus = busRepository.save(busDb);
+        // Checking - assertion
+        assertThat(updatedBus.getName()).isEqualTo("Nellai Express");
+    }
+
+    @Test
+    public void givenBusObject_whenDeleteBus_thenRemoveBus() {
+        Bus savedBus = busRepository.save(bus);
+        busRepository.deleteById(savedBus.getId());
+
+        Optional<Bus> busOptional = busRepository.findById(savedBus.getId());
+
+        // Checking - assertion
+        assertThat(busOptional).isEmpty();
+    }
 }
